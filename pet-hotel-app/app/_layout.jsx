b4eddb56eac1aa702f98765a1d26e02c0a9414c3
Bduyy2703 <nguyenduy7003@gmail.com>
2025-04-15 18:52:36 +0700

    update image

--- pet-hotel-app/app/_layout.jsx
+++ pet-hotel-app/app/_layout.jsx
@@ -1,36 +1,13 @@
-import { ClerkProvider, ClerkLoaded } from "@clerk/clerk-expo";
-import * as SecureStore from "expo-secure-store";
-import { useFonts } from "expo-font";
-import { Stack } from "expo-router";
+import { useFonts } from 'expo-font';
+import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
 
-const tokenCache = {
-  async getToken(key) {
-    try {
-      const item = await SecureStore.getItemAsync(key);
-      if (item) {
-        console.log(`${key} was used 🔐 \n`);
-      } else {
-        console.log("No values stored under key: " + key);
-      }
-      return item;
-    } catch (error) {
-      console.error("SecureStore get item error: ", error);
-      await SecureStore.deleteItemAsync(key);
-      return null;
-    }
-  },
-  async saveToken(key, value) {
-    try {
-      return SecureStore.setItemAsync(key, value);
-    } catch (err) {
-      return;
-    }
-  },
-};
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
-
   const [loaded] = useFonts({
     "open-sans": require("./../assets/fonts/OpenSans-Regular.ttf"),
     "open-sans-bold": require("./../assets/fonts/OpenSans-Bold.ttf"),
@@ -40,208 +17,238 @@ export default function RootLayout() {
     "nunito-medium": require("./../assets/fonts/Nunito-Medium.ttf"),
   });
 
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
   if (!loaded) {
     return null;
   }
 
   return (
-    <ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey}>
-      <Stack
-        screenOptions={{
-          headerTitleAlign: "center",
-          headerTitleStyle: {
-            fontFamily: "nunito-bold",
-            color: "#4EA0B7",
-            fontSize: 28,
-          },
-          headerStyle: {
-            backgroundColor: "#FDFBF6",
-          },
+    <Stack
+      screenOptions={{
+        headerTitleAlign: "center",
+        headerTitleStyle: {
+          fontFamily: "nunito-bold",
+          color: "#4EA0B7",
+          fontSize: 28,
+        },
+        headerStyle: {
+          backgroundColor: "#FDFBF6",
+        },
+      }}
+    >
+      <Stack.Screen name="index"
+        options={{
+          headerShown: false,
+        }} />
+      <Stack.Screen
+        name="(tabs)"
+        options={{
+          headerShown: false,
         }}
-      >
-        <Stack.Screen name="index" />
-        <Stack.Screen
-          name="(tabs)"
-          options={{
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen
-          name="login/index"
-          options={{
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen
-          name="search/SearchResult"
-          options={{
-            title: "Search Results",
-          }}
-        />
-        <Stack.Screen
-          name="screen/login"
-          options={{
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen
-          name="screen/signup"
-          options={{
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen
-          name="screen/verify"
-          options={{
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen
-          name="screen/init_profile"
-          options={{
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen
-          name="screen/settings"
-          options={{
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen
-          name="screen/profile"
-          options={{
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen
-          name="screen/changePassword"
-          options={{
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen
-          name="screen/forgotPassword"
-          options={{
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen
-          name="screen/resetPassword"
-          options={{
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen
-          name="screen/message"
-          options={{
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen
-          name="screen/newMessage"
-          options={{
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen
-          name="screen/group"
-          options={{
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen
-          name="screen/chat"
-          options={{
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen
-          name="screen/notifications"
-          options={{
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen
-          name="screen/profileChat"
-          options={{
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen
-          name="screen/evaluate"
-          options={{
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen
-          name="screen/createEvaluate"
-          options={{
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen
-          name="screen/confirmBooking"
-          options={{
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen
-          name="screen/booking"
-          options={{
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen
-          name="screen/details"
-          options={{
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen
-          name="screen/bookingSuccess"
-          options={{
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen
-          name="screen/pet"
-          options={{
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen
-          name="screen/search"
-          options={{
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen
-          name="screen/success"
-          options={{
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen
-          name="screen/cancel"
-          options={{
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen
-          name="screen/payment"
-          options={{
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen
-          name="screen/resultScreen"
-          options={{
-            headerShown: false,
-          }}
-        />
-      </Stack>
-      
-    </ClerkProvider>
+      />
+      <Stack.Screen
+        name="login/index"
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="search/SearchResult"
+        options={{
+          title: "Search Results",
+        }}
+      />
+      <Stack.Screen
+        name="screen/login"
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="screen/signup"
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="screen/verify"
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="screen/init_profile"
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="screen/settings"
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="screen/profile"
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="screen/changePassword"
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="screen/forgotPassword"
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="screen/resetPassword"
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="screen/message"
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="screen/newMessage"
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="screen/group"
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="screen/chat"
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="screen/notifications"
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="screen/profileChat"
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="screen/evaluate"
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="screen/createEvaluate"
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="screen/confirmBooking"
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="screen/booking"
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="screen/details"
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="screen/bookingSuccess"
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="screen/pet"
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="screen/search"
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="screen/success"
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="screen/cancel"
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="screen/payment"
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="screen/resultScreen"
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="screen/premium"
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="screen/schedule"
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="screen/update_pet"
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="screen/chatMessage"
+        options={{
+          headerShown: false,
+        }}
+      />
+    </Stack>
   );
 }
--- pet-hotel-app/app/index.jsx
+++ pet-hotel-app/app/index.jsx
@@ -1,25 +1,115 @@
-import { useUser } from "@clerk/clerk-expo";
-import { Link, Redirect } from "expo-router";
-import { Text, View } from "react-native";
+import { View, Text, Image, Pressable } from 'react-native'
+import React, { useCallback } from 'react'
+import * as Linking from 'expo-linking'
+import * as WebBrowser from 'expo-web-browser'
+import { router } from 'expo-router'
+import { useNavigation } from '@react-navigation/native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { commonStyles } from '../style';
 
-export default function Index() {
+export const useWarmUpBrowser = () => {
+  React.useEffect(() => {
+    // Warm up the android browser to improve UX
+    // https://docs.expo.dev/guides/authentication/#improving-user-experience
+    void WebBrowser.warmUpAsync()
+    return () => {
+      void WebBrowser.coolDownAsync()
+    }
+  }, [])
+}
 
-  const {user} = useUser();
+WebBrowser.maybeCompleteAuthSession()
+
+export default function LoginScreen() {
+  const navigation = useNavigation();
+
+  const handleLogin = () => {
+    router.push('screen/login');
+  }
+
+
+  
 
   return (
-    <View
-      style={{
+    <SafeAreaView style={
+      // [commonStyles.container, {paddingTop:-50, backgroundColor :'#EDF8FD'}]
+      {
+        backgroundColor: '#EDF8FD',
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        // paddingTop: 50
+      }
+    }>
+      <Text style={{
+        margin: 25,
+        fontSize: 28,
+        textAlign: 'center',
+        // marginTop: 60,
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Welcome {user?.fullName}</Text>
-      {user?
-      <Redirect href={'/(tabs)/home'} /> :
-      <Redirect href={'/login'} />
-    }
-       
-    </View>
-  );
+        color: '#3070B3',
+        fontFamily: 'nunito-bold',
+      }}>
+        Chào mừng đến với <Text style={{ fontSize: 35 }}>ApeHome!</Text>
+      </Text>
+
+      {/* <Text style={{ fontSize: 35, textAlign: 'center', color: '#3070B3', fontFamily: 'nunito-bold', }}>ApeHome!</Text> */}
+      <View style={{ width: '80%', flex:6, alignItems: 'center' }}>
+        <Image
+          source={require('../assets/images/logo.png')}
+          style={{ width:400, height:400}}
+          resizeMode="contain"
+        />
+        <Text style={{
+          fontSize: 20,
+          fontWeight:'bold',
+          marginLeft: 15,
+          marginRight: 15,
+          // padding: 30,
+          fontFamily: 'nunito',
+          // color: '#4EA0B7',
+          color: '#416FAE',
+          textAlign: 'center',
+          // }}>Where all of your pets’ needs are right here!</Text>
+        }}>Nơi đáp ứng mọi nhu cầu cho thú cưng của bạn!</Text>
+      </View>
+
+
+      <View style={{ flex: 2 }}>
+        <Pressable onPress={() => router.push('/screen/signup')} style={{
+          padding: 13,
+          marginTop: 40,
+          backgroundColor: '#416FAE',
+          borderRadius: 50,
+          width: 300,
+        }}>
+          <Text style={{
+            textAlign: 'center',
+            fontSize: 18,
+            color: '#fff',
+            fontFamily: 'nunito-bold',
+          }}>Tạo tài khoản</Text>
+        </Pressable>
+        <Pressable onPress={handleLogin} style={{
+          padding: 13,
+          marginTop: 10,
+          borderWidth:1,
+          borderColor:'#416FAE',
+          // backgroundColor: '#416FAE',
+          borderRadius: 50,
+          width: 300,
+        }}>
+          <Text style={{
+            textAlign: 'center',
+            fontSize: 18,
+            color: '#416FAE',
+            fontFamily: 'nunito-bold',
+          }}>Đăng nhập</Text>
+        </Pressable>
+      </View>
+
+    </SafeAreaView>
+  )
 }

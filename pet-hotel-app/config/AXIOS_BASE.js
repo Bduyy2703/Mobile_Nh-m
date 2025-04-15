bb40456b1c1e9ceeabb98bd239e210f4d5165f7c
Bduyy2703 <nguyenduy7003@gmail.com>
2025-04-15 10:45:58 +0700

    fix auth

--- pet-hotel-app/config/AXIOS_BASE.js
+++ pet-hotel-app/config/AXIOS_BASE.js
@@ -2,8 +2,9 @@ import axios from "axios";
 
 const config = {
     // baseURL: "http://192.168.1.7:8080/no-auth", // nha C7
-    baseURL: "http://192.168.7.177:8080/no-auth", // nha Tien
+    // baseURL: "http://192.168.7.177:8080/no-auth", // nha Tien
     // baseURL: "http://10.87.24.104:8080/no-auth", // nha Tien
+    baseURL: "http://10.5.50.156:9090/no-auth", // IPhone 13
     header: {
         "Content-Type": "application/json",
     }

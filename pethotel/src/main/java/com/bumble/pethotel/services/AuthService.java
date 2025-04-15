05253bec9a1f59c111dc95b16018156575ff5267
Bduyy2703 <nguyenduy7003@gmail.com>
2025-04-15 09:37:45 +0700

    update for api auth - refactor src and config cloudinary

--- pethotel/src/main/java/com/bumble/pethotel/services/AuthService.java
+++ pethotel/src/main/java/com/bumble/pethotel/services/AuthService.java
@@ -11,5 +11,12 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public interface AuthService {
-
+    AuthenticationResponse login(LoginDto loginDto);
+    String signup(SignupDto signupDto);
+    String signupOwner(SignupDto signupDto);
+    AuthenticationResponse refreshToken(HttpServletRequest request, HttpServletResponse response) throws IOException;
+    String verifyEmailCode(String email, String code);
+    String resendVerificationCode(String email);
+    String forgotPassword(String email);
+    String resetPassword(NewPasswordRequest newPasswordRequest);
 }

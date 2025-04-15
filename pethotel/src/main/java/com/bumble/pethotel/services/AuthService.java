d01bbc51dd2ba8e25763d789991d9776f07ca660
Bduyy2703 <nguyenduy7003@gmail.com>
2025-04-15 09:33:41 +0700

    init refactor for auth

--- pethotel/src/main/java/com/bumble/pethotel/services/AuthService.java
+++ pethotel/src/main/java/com/bumble/pethotel/services/AuthService.java
@@ -11,12 +11,5 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public interface AuthService {
-    AuthenticationResponse login(LoginDto loginDto);
-    String signup(SignupDto signupDto);
-    String signupOwner(SignupDto signupDto);
-    AuthenticationResponse refreshToken(HttpServletRequest request, HttpServletResponse response) throws IOException;
-    String verifyEmailCode(String email, String code);
-    String resendVerificationCode(String email);
-    String forgotPassword(String email);
-    String resetPassword(NewPasswordRequest newPasswordRequest);
+
 }
